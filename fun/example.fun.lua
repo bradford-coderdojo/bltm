4fd7e0ffe691f11a1ffab6e23176bb9f27c03c0b
Kriss <kriss@xixs.com>
2017-02-18 10:32:49 +0000

    Update example.fun.lua

--- fun/example.fun.lua
+++ fun/example.fun.lua
@@ -1,3 +1,11 @@
+--[[
+
+Run this file online by visiting
+
+https://xriss.github.io/fun64/run/?url=https%3A%2F%2Fraw.githubusercontent.com%2Fbradford-coderdojo%2Fbltm%2Fmaster%2Ffun%2Fexample.fun.lua
+
+]]
+
 
 local bit=require("bit")
 local wstr=require("wetgenes.string")
