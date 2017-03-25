a93a7b7fcd9d8b46a3b2c5101d5bd3ccaa6f7b3b
Ben Wyatt <benjaminjosephwyatt@ymail.com>
2017-03-25 11:16:27 +0000

    Update ben.chatdown.fun.lua

--- team/ben/ben.chatdown.fun.lua
+++ team/ben/ben.chatdown.fun.lua
@@ -514,7 +514,7 @@ local setup_chat=function(chat,chats,chat_name,response_name)
 
 			local ss=v and v.text or {} if type(ss)=="string" then ss={ss} end
 
-			local color=10
+			local color=30
 			if chat.viewed[v.name] then color=2 end -- we have already seen the response to this decision
 			
 			local f=function(item,menu)
